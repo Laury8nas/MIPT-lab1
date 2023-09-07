d8712a272e36ec4ba2ed480aab385ed0fde3145e
Laury8nas <pivoraslaurynas@gmail.com>
2023-09-07 21:18:43 +0300

    Pridedamas pirmasis mygtukas

--- app/src/main/java/com/example/miptlab1/MainActivity.kt
+++ app/src/main/java/com/example/miptlab1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.miptlab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.miptlab1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnChangeText.setOnClickListener {
+            binding.tvField.text = "Labas rytas!"
+        }
     }
 }
